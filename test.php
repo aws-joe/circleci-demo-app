46a584a536100ed23c32e15c5e067b7c8361d971
aws-joe <fontesj@amazon.com>
2015-04-01 15:04:47 -0700

    fourth iteration

--- test.php
+++ test.php
@@ -2,6 +2,20 @@
 
 chdir("www/");
 
+ob_start();
+
 include "./index.php";
+
+$buff = ob_get_clean();
+
+if(strlen($buff) < 8000)
+        {
+        print "Error in system information gathering<br>\n";
+        exit(1);
+        }
+else
+	{
+	print "Buffer Size: ".strlen($buff)."\n<br>";
+	}
 
 ?>
